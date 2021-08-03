43957b36f3bf4aff222624f76ca920133911df3f
Lavish Thakkar <lavisht22@gmail.com>
2021-08-03 13:42:26 +0530

    Update DB Config

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
