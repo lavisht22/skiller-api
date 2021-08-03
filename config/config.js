363f16064cf85ca078e4dbd38cb386b58da697e3
Lavish Thakkar <lavisht22@gmail.com>
2021-08-03 16:33:30 +0530

    Update sequelize config

--- config/config.js
+++ config/config.js
@@ -1,25 +1,11 @@
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    url: 'postgres://lavisht22@localhost/lavisht22?ssl=true',
     dialect: 'postgres',
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  },
+  test: { url: process.env.DATABASE_URL, dialect: 'postgres' },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    url: `${process.env.DATABASE_URL}?ssl=true`,
     dialect: 'postgres',
-    ssl: true,
   },
 };
